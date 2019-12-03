46491e8b2e984920998e3351212ae34562671ea8
orn28 <49461672+orn28@users.noreply.github.com>
2019-12-03 14:07:24 -0800

    Update lab9.sh

--- lab9.sh
+++ lab9.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+args=( "$@" )
+n_args=${#args[@]}
+sum=0
+for (( i=1; i<n_args+1; i++ ));
+do
+   sum=$((sum+args[i-1]))
+done
+
+echo -n "average of the $n_args numbers is: "
+echo "$sum / $n_args.0" | bc -l
